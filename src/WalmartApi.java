bddda82c6c8a55771cd19ea66ac22425c6de7436
greentay20 <greenwaldtaylor@gmail.com>
2026-01-04 21:31:28 -0500

    Retrieve specific items at specific stores
    - Retrieve data from specific items at specific stores

---
+++ src/WalmartApi.java
@@ -0,0 +1,79 @@
+package src;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class WalmartApi {
+
+    public static JsonNode getItemsByStoreId(String[] itemIds, String storeId) throws Exception {
+
+        // id and keys
+        String consumerId = System.getenv("WM_CONSUMER_ID");
+        String keyVersion = System.getenv("WM_KEY_VERSION");
+        String privateKey = System.getenv("WM_PRIVATE_KEY");
+        
+        String url = "";
+        if (itemIds.length == 1) {
+            url =
+                "https://developer.api.walmart.com/api-proxy/service/affil/product/v2/items/" +
+                itemIds[0] +
+                "?storeId=" + storeId;
+        } else {
+            String itemIdsStr = "";
+            for (int i = 0; i < itemIds.length - 1; i++) {
+                itemIdsStr += (itemIds[i] + ",");
+            }
+            itemIdsStr += itemIds[itemIds.length - 1];
+
+            url = "https://developer.api.walmart.com/api-proxy/service/affil/product/v2/items?" +
+                "ids=" + itemIdsStr +
+                "&storeId=" + storeId;
+        }
+        
+        
+        // unix timestamp
+        String timestamp = String.valueOf(System.currentTimeMillis());
+
+        // headers
+        Map<String, String> headers = new HashMap<>();
+        headers.put("WM_CONSUMER.ID", consumerId);
+        headers.put("WM_CONSUMER.INTIMESTAMP", timestamp);
+        headers.put("WM_SEC.KEY_VERSION", keyVersion);
+
+        // sign headers
+        SignatureGenerator signer = new SignatureGenerator();
+        String[] canonical = SignatureGenerator.canonicalize(headers);
+        String signature = signer.generateSignature(privateKey, canonical[1]);
+
+        // http request
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(url))
+            .GET()
+            .header("WM_CONSUMER.ID", consumerId)
+            .header("WM_CONSUMER.INTIMESTAMP", timestamp)
+            .header("WM_SEC.KEY_VERSION", keyVersion)
+            .header("WM_SEC.AUTH_SIGNATURE", signature)
+            .build();
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        
+        // get structured json data
+        if (response.statusCode() != 200) { // store id doesnt exist anymore or smth else wrong
+            return null;
+        }
+
+        String prodInfo = response.body();
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(prodInfo);
+        return root;
+    }
+
+}
+
